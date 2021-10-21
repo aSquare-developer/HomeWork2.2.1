1c20a583aeeb314b7c69dbd88cf77209a905b77f
Artur <artur.anissimov@gmail.com>
2021-10-22 01:20:57 +0300

    .

--- HomeWork2.2.1/ViewController.swift
+++ HomeWork2.2.1/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redColorValueLabel: UILabel!
+    @IBOutlet weak var greenColorValueLabel: UILabel!
+    @IBOutlet weak var blueColorValueLabel: UILabel!
+    
+    @IBOutlet weak var redColorHSlider: UISlider!
+    @IBOutlet weak var greenColorHSlider: UISlider!
+    @IBOutlet weak var blueColorHSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    override func viewWillLayoutSubviews() {
+        colorView.layer.cornerRadius = 15
+        redColorHSlider.minimumTrackTintColor = .red
+        greenColorHSlider.minimumTrackTintColor = .green
+        blueColorHSlider.minimumTrackTintColor = .blue
+        
+        changeColor()
 
+    }
+    
+    func changeColor() {
+        colorView.backgroundColor = UIColor(displayP3Red: CGFloat(redColorHSlider.value),
+                                            green: CGFloat(greenColorHSlider.value),
+                                            blue: CGFloat(blueColorHSlider.value),
+                                            alpha: 1)
+    }
+    
+    @IBAction func rgbSliderChanged(_ sender: Any) {
+        redColorValueLabel.text = String(format: "%.2f", redColorHSlider.value)
+        greenColorValueLabel.text = String(format: "%.2f", greenColorHSlider.value)
+        blueColorValueLabel.text = String(format: "%.2f", blueColorHSlider.value)
+        
+        changeColor()
+    }
+    
+    
 }
 
